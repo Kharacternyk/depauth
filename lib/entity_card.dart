c1631a2461e6a4671774cab4e5d36a70cb267500
Nazar Vinnichuk <nazar@vinnich.uk>
2023-07-05 17:10:10 +0300

    Fix unbalanced parentheses.

--- lib/entity_card.dart
+++ lib/entity_card.dart
@@ -15,46 +15,45 @@ class EntityCard extends StatelessWidget {
 
     return FractionalPadding(
       childSizeFactor: 6,
-        child: Card(
-          elevation: 10,
-          margin: EdgeInsets.zero,
-          shape: const Border(),
-          child: Column(
-            children: [
-              Expanded(
-                child: Material(
-                  color: colors.primaryContainer,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: FittedBox(
-                          child: Padding(
-                            padding: padding,
-                            child: Icon(
-                              switch (entity.type) {
-                                EntityType.hardwareKey => Icons.key,
-                                EntityType.webService => Icons.web,
-                                EntityType.person => Icons.person
-                              },
-                              color: colors.onPrimaryContainer,
-                            ),
+      child: Card(
+        elevation: 10,
+        margin: EdgeInsets.zero,
+        shape: const Border(),
+        child: Column(
+          children: [
+            Expanded(
+              child: Material(
+                color: colors.primaryContainer,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: FittedBox(
+                        child: Padding(
+                          padding: padding,
+                          child: Icon(
+                            switch (entity.type) {
+                              EntityType.hardwareKey => Icons.key,
+                              EntityType.webService => Icons.web,
+                              EntityType.person => Icons.person
+                            },
+                            color: colors.onPrimaryContainer,
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              Expanded(
-                child: FittedBox(
-                  child: Container(
-                    padding: padding,
-                    child: Text(entity.name),
-                  ),
+            ),
+            Expanded(
+              child: FittedBox(
+                child: Container(
+                  padding: padding,
+                  child: Text(entity.name),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
