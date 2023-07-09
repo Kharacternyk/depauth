f5e8bd101bd950cb909b01973984f98736e0dbc5
Nazar Vinnichuk <nazar@vinnich.uk>
2023-07-09 23:44:26 +0300

    Cache entities, not widgets.

--- lib/entity_graph.dart
+++ lib/entity_graph.dart
@@ -19,7 +19,7 @@ class EntityGraph extends StatefulWidget {
 
 class _State extends State<EntityGraph> {
   final Db db = Db();
-  final Map<Position, Widget> children = {};
+  final Map<Position, ({String name, EntityType type})?> entities = {};
   (Position, Position)? boundaries;
 
   @override
@@ -41,68 +41,59 @@ class _State extends State<EntityGraph> {
       final row = <Widget>[];
       for (var x = start.x - 1; x <= end.x + 1; ++x) {
         final position = Position(x, y);
-        final Widget child;
 
-        switch (children[Position(x, y)]) {
-          case Widget widget:
-            child = widget;
-            break;
-          case null:
-            child = switch (db.getEntityByPosition(position)) {
-              ({String name, EntityType type}) entity => Expanded(
-                  child: ScaledDraggable<Position>(
-                    scale: widget.scale,
-                    dragData: position,
-                    child: EntityCard(name: entity.name, type: entity.type),
-                    wrapDragged: (child) =>
-                        FractionalPadding(childSizeFactor: 6, child: child),
-                    wrapPlaced: (child) => FractionalPadding(
-                      childSizeFactor: 6,
-                      child: ArrowElement(
-                        id: entity.name,
-                        color: Theme.of(context)
-                            .colorScheme
-                            .primary
-                            .withOpacity(0.5),
-                        sourceAnchor: Alignment.topCenter,
-                        targetAnchor: Alignment.bottomCenter,
-                        tipLength: 0,
-                        width: 4,
-                        child: child,
-                      ),
-                    ),
-                  ),
-                ),
-              null => EntityPlaceholder<Position>(
-                  onDragAccepted: (oldPosition) => setState(() {
-                    children.remove(oldPosition);
-                    children.remove(position);
-                    boundaries = null; // FIXME
-                    db.setEntityPosition(
-                        newPosition: position, oldPosition: oldPosition);
-                  }),
-                  icon: switch ((
-                    (x - end.x, y - end.y),
-                    (start.x - x, start.y - y)
-                  )) {
-                    ((1, 1), _) => Icons.south_east,
-                    (_, (1, 1)) => Icons.north_west,
-                    ((_, 1), (1, _)) => Icons.south_west,
-                    ((1, _), (_, 1)) => Icons.north_east,
-                    ((_, 1), _) => Icons.south,
-                    ((1, _), _) => Icons.east,
-                    (_, (_, 1)) => Icons.north,
-                    (_, (1, _)) => Icons.west,
-                    _ => null,
-                  },
-                ),
-            };
-            children[position] = child;
-            break;
+        if (!entities.containsKey(position)) {
+          entities[position] = db.getEntityByPosition(position);
         }
 
-        row.add(child);
+        row.add(switch (entities[position]) {
+          ({String name, EntityType type}) entity => Expanded(
+              child: ScaledDraggable<Position>(
+                scale: widget.scale,
+                dragData: position,
+                child: EntityCard(name: entity.name, type: entity.type),
+                wrapDragged: (child) =>
+                    FractionalPadding(childSizeFactor: 6, child: child),
+                wrapPlaced: (child) => FractionalPadding(
+                  childSizeFactor: 6,
+                  child: ArrowElement(
+                    id: entity.name,
+                    color:
+                        Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                    sourceAnchor: Alignment.topCenter,
+                    targetAnchor: Alignment.bottomCenter,
+                    tipLength: 0,
+                    width: 4,
+                    child: child,
+                  ),
+                ),
+              ),
+            ),
+          null => EntityPlaceholder<Position>(
+              onDragAccepted: (oldPosition) => setState(() {
+                entities[position] = entities.remove(oldPosition);
+                boundaries = null; // FIXME
+                db.setEntityPosition(
+                    newPosition: position, oldPosition: oldPosition);
+              }),
+              icon: switch ((
+                (x - end.x, y - end.y),
+                (start.x - x, start.y - y)
+              )) {
+                ((1, 1), _) => Icons.south_east,
+                (_, (1, 1)) => Icons.north_west,
+                ((_, 1), (1, _)) => Icons.south_west,
+                ((1, _), (_, 1)) => Icons.north_east,
+                ((_, 1), _) => Icons.south,
+                ((1, _), _) => Icons.east,
+                (_, (_, 1)) => Icons.north,
+                (_, (1, _)) => Icons.west,
+                _ => null,
+              },
+            ),
+        });
       }
+
       rows.add(Expanded(child: Row(children: row)));
     }
 
