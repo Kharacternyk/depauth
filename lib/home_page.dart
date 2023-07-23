3eff8cd12044f5f4f9ccec1166248ec76b041f2e
Nazar Vinnichuk <nazar@vinnich.uk>
2023-07-23 12:59:08 +0300

    Split the home page into a file.

---
+++ lib/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:jovial_svg/jovial_svg.dart';
+import 'package:path/path.dart';
+
+import 'async_resources.dart';
+import 'core/entity_source.dart';
+import 'entity_graph.dart';
+import 'scaled_draggable.dart';
+import 'viewer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  createState() => _State();
+}
+
+class _State extends State<HomePage> {
+  Widget? sideBar;
+
+  @override
+  build(BuildContext context) {
+    const addEntityTooltip =
+        "Drag this button onto an empty space to create a new entity.";
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+        title: ScalableImageWidget.fromSISource(
+          si: ScalableImageSource.fromSI(
+            DefaultAssetBundle.of(context),
+            'assets/logo.si',
+          ),
+          scale: 0.1,
+        ),
+      ),
+      body: Viewer(
+        minScale: 1,
+        maxScale: 20,
+        child: EntityGraph(
+          join(
+            AsyncResources.of(context).documentsDirectory,
+            'personal.depauth',
+          ),
+        ),
+      ),
+      floatingActionButton: ScaledDraggable(
+        dragData: const NewEntitySource(),
+        child: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text(addEntityTooltip),
+                showCloseIcon: true,
+              ),
+            );
+          },
+          tooltip: addEntityTooltip,
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:jovial_svg/jovial_svg.dart';
-import 'package:path/path.dart';
 
 import 'async_resources.dart';
-import 'core/entity_source.dart';
-import 'entity_graph.dart';
-import 'scaled_draggable.dart';
-import 'viewer.dart';
+import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,65 +23,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'DepAuth'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
-
-  final String title;
-
-  @override
-  build(BuildContext context) {
-    const addEntityTooltip =
-        "Drag this button onto an empty space to create a new entity.";
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Row(
-          children: [
-            ScalableImageWidget.fromSISource(
-              si: ScalableImageSource.fromSI(
-                DefaultAssetBundle.of(context),
-                'assets/logo.si',
-              ),
-              scale: 0.1,
-            ),
-            Text(title),
-          ],
-        ),
-      ),
-      body: Viewer(
-        minScale: 1,
-        maxScale: 20,
-        child: EntityGraph(
-          join(
-            AsyncResources.of(context).documentsDirectory,
-            'personal.depauth',
-          ),
-        ),
-      ),
-      floatingActionButton: ScaledDraggable(
-        dragData: const NewEntitySource(),
-        child: FloatingActionButton(
-          onPressed: () {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text(addEntityTooltip),
-                showCloseIcon: true,
-              ),
-            );
-          },
-          tooltip: addEntityTooltip,
-          child: const Icon(Icons.add),
-        ),
-      ),
+      home: const HomePage(),
     );
   }
 }
