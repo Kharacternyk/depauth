0f14f9351889f4adda9f454ed5a3ad0fb85bf79a
Nazar Vinnichuk <nazar@vinnich.uk>
2023-09-11 18:01:37 +0300

    Rename ControlPanel to StoragePanel.

---
+++ lib/storage_panel.dart
@@ -0,0 +1,221 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/messages.dart';
+
+import 'application_storage.dart';
+import 'core/position.dart';
+import 'core/storage_insight.dart';
+import 'core/traveler.dart';
+import 'core/traversable_entity.dart';
+import 'entity_form.dart';
+import 'entity_graph.dart';
+import 'menu_drawer.dart';
+import 'scaled_draggable.dart';
+import 'split_view.dart';
+import 'storage_form.dart';
+import 'viewer.dart';
+import 'widget_extension.dart';
+
+class StoragePanel extends StatefulWidget {
+  final String storagePath;
+
+  const StoragePanel(this.storagePath, {super.key});
+
+  @override
+  createState() => _State();
+}
+
+class _State extends State<StoragePanel> {
+  final editablePosition = ValueNotifier<Position?>(null);
+  final formHasTraveler = ValueNotifier(false);
+  late var storage = ApplicationStorage(widget.storagePath);
+
+  @override
+  dispose() {
+    storage.dispose();
+    editablePosition.dispose();
+    formHasTraveler.dispose();
+    super.dispose();
+  }
+
+  @override
+  deactivate() {
+    storage.dispose();
+    super.deactivate();
+  }
+
+  @override
+  activate() {
+    super.activate();
+    storage = ApplicationStorage(widget.storagePath);
+  }
+
+  @override
+  build(BuildContext context) {
+    final messages = AppLocalizations.of(context)!;
+    final colors = Theme.of(context).colorScheme;
+    final storageForm = (StorageInsight insight) {
+      return StorageForm(
+        insight: insight,
+        resetLoss: storage.resetLoss,
+        resetCompromise: storage.resetCompromise,
+      );
+    }.listen(storage.storageInsight);
+
+    return Scaffold(
+      drawer: const MenuDrawer(),
+      body: Stack(
+        children: [
+          (bool formHasTraveler) {
+            return Ink(
+              color: formHasTraveler
+                  ? colors.primaryContainer
+                  : colors.surfaceVariant,
+            );
+          }.listen(formHasTraveler),
+          SplitView(
+            mainChild: Material(
+              child: Viewer(
+                minScale: 1,
+                maxScale: 20,
+                child: EntityGraph(
+                  storage,
+                  setEditablePosition: (position) {
+                    editablePosition.value = position;
+                  },
+                ),
+              ),
+            ),
+            sideChild: (Position? position) {
+              switch (position) {
+                case null:
+                  return storageForm;
+                case Position position:
+                  final listenableEntity =
+                      storage.getListenableEntity(position);
+
+                  return (TraversableEntity? entity) {
+                    return switch (entity) {
+                      TraversableEntity entity => () {
+                          return EntityForm(
+                            entity,
+                            position: position,
+                            hasTraveler: formHasTraveler,
+                            goBack: () {
+                              editablePosition.value = null;
+                            },
+                            insight: storage.getEntityInsight(entity.identity),
+                            changeName: (name) {
+                              storage.changeName(position, name);
+                            },
+                            changeType: (type) {
+                              storage.changeType(position, type);
+                            },
+                            toggleLost: (value) {
+                              storage.toggleLost(position, value);
+                            },
+                            toggleCompromised: (value) {
+                              storage.toggleCompromised(position, value);
+                            },
+                            addDependency: (factor, entity) {
+                              storage.addDependency(
+                                position,
+                                factor,
+                                entity,
+                              );
+                            },
+                            addDependencyAsFactor: (dependency) {
+                              storage.addDependencyAsFactor(
+                                position,
+                                entity: entity.identity,
+                                dependency: dependency,
+                              );
+                            },
+                            removeDependency: (factor, entity) {
+                              storage.removeDependency(
+                                position,
+                                factor,
+                                entity,
+                              );
+                            },
+                            addFactor: () {
+                              storage.addFactor(position, entity.identity);
+                            },
+                          );
+                        }.listen(storage.entityInsightNotifier),
+                      null => storageForm,
+                    };
+                  }.listen(listenableEntity);
+              }
+            }.listen(editablePosition),
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: [
+          const DrawerButton(),
+          Text(
+            widget.storagePath,
+            overflow: TextOverflow.fade,
+            softWrap: false,
+          ).tip(widget.storagePath).expand(),
+          DragTarget<DeletableTraveler>(
+            builder: (context, candidate, rejected) {
+              return FloatingActionButton(
+                backgroundColor:
+                    candidate.isNotEmpty ? colors.error : colors.errorContainer,
+                foregroundColor: candidate.isNotEmpty
+                    ? colors.onError
+                    : colors.onErrorContainer,
+                onPressed: () {
+                  ScaffoldMessenger.of(context)
+                    ..hideCurrentSnackBar()
+                    ..showSnackBar(
+                      SnackBar(
+                        content: Text(messages.deleteButtonTooltip),
+                        showCloseIcon: true,
+                      ),
+                    );
+                },
+                tooltip: messages.deleteButtonTooltip,
+                child: const Icon(Icons.delete),
+              );
+            },
+            onAccept: (traveler) {
+              switch (traveler) {
+                case EntityTraveler traveler:
+                  storage.deleteEntity(traveler.position);
+                case FactorTraveler traveler:
+                  storage.removeFactor(traveler.position, traveler.factor);
+                case DependencyTraveler traveler:
+                  storage.removeDependency(
+                    traveler.position,
+                    traveler.factor,
+                    traveler.entity,
+                  );
+              }
+            },
+          ),
+          const SizedBox(width: 8),
+          ScaledDraggable(
+            dragData: const CreationTraveler(),
+            child: FloatingActionButton(
+              onPressed: () {
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(
+                    SnackBar(
+                      content: Text(messages.addButtonTooltip),
+                      showCloseIcon: true,
+                    ),
+                  );
+              },
+              tooltip: messages.addButtonTooltip,
+              mouseCursor: SystemMouseCursors.grab,
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ].row.group,
+      ),
+    );
+  }
+}
