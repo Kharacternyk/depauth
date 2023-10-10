1ac2b7795c3ef4e36d2ef5def0a361e53c174635
Nazar Vinnichuk <nazar@vinnich.uk>
2023-10-10 20:43:31 +0300

    Replace a ternary expression with an if block.

--- lib/entity_placeholder.dart
+++ lib/entity_placeholder.dart
@@ -22,31 +22,35 @@ class EntityPlaceholder<DragDataType extends Object> extends StatelessWidget {
     final colors = Theme.of(context).colorScheme;
     final messages = AppLocalizations.of(context)!;
     final isSingleton = boundaries.start == boundaries.end;
-    final child = isSingleton
-        ? Center(
-            child: Text(
-              messages.singletonPlaceholderTip,
-              textAlign: TextAlign.center,
-            ),
-          ).pad(const EdgeInsets.all(8))
-        : Icon(
-            switch ((
-              (position.x - boundaries.end.x, position.y - boundaries.end.y),
-              (boundaries.start.x - position.x, boundaries.start.y - position.y)
-            )) {
-              ((0, 0), (0, 0)) => null,
-              ((0, 0), _) => Icons.south_east,
-              (_, (0, 0)) => Icons.north_west,
-              ((_, 0), (0, _)) => Icons.south_west,
-              ((0, _), (_, 0)) => Icons.north_east,
-              ((_, 0), _) => Icons.south,
-              ((0, _), _) => Icons.east,
-              (_, (_, 0)) => Icons.north,
-              (_, (0, _)) => Icons.west,
-              _ => null,
-            },
-            color: colors.onPrimaryContainer,
-          ).fit;
+    final Widget child;
+
+    if (isSingleton) {
+      child = Center(
+        child: Text(
+          messages.singletonPlaceholderTip,
+          textAlign: TextAlign.center,
+        ),
+      ).pad(const EdgeInsets.all(8));
+    } else {
+      child = Icon(
+        switch ((
+          (position.x - boundaries.end.x, position.y - boundaries.end.y),
+          (boundaries.start.x - position.x, boundaries.start.y - position.y)
+        )) {
+          ((0, 0), (0, 0)) => null,
+          ((0, 0), _) => Icons.south_east,
+          (_, (0, 0)) => Icons.north_west,
+          ((_, 0), (0, _)) => Icons.south_west,
+          ((0, _), (_, 0)) => Icons.north_east,
+          ((_, 0), _) => Icons.south,
+          ((0, _), _) => Icons.east,
+          (_, (_, 0)) => Icons.north,
+          (_, (0, _)) => Icons.west,
+          _ => null,
+        },
+        color: colors.onPrimaryContainer,
+      ).fit;
+    }
 
     return DragTarget(
       builder: (context, candidate, rejected) {
