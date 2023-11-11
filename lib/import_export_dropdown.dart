b759771696ccca6e2003785e9dd153156f7e80b1
Nazar Vinnichuk <nazar@vinnich.uk>
2023-11-11 19:59:27 +0200

    Use a friendly name for Android exports.

--- lib/import_export_dropdown.dart
+++ lib/import_export_dropdown.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/messages.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 import 'card_dropdown.dart';
@@ -60,15 +62,25 @@ class ImportExportDropdown extends StatelessWidget {
           leading: const Icon(Icons.east),
           title: Text(messages.exportStorage),
           onTap: () async {
-            final name = getName() + applicationFileExtension;
             late final storage = slot.export().writeToBuffer();
 
-            if (theme.platform == TargetPlatform.android ||
-                theme.platform == TargetPlatform.iOS) {
-              await Share.shareXFiles([
-                XFile.fromData(storage, name: name),
-              ]);
+            if (Platform.isAndroid || Platform.isIOS) {
+              final cacheDirectory = await getApplicationCacheDirectory();
+              final file = await File(join(
+                cacheDirectory.path,
+                messages.exportedStorageName +
+                    messages.applicationFileExtension,
+              )).open(mode: FileMode.append);
+
+              await file.lock(FileLock.blockingExclusive);
+              await file.setPosition(0);
+              await file.truncate(0);
+              await file.writeFrom(storage);
+              await file.unlock();
+              await file.close();
+              await Share.shareXFiles([XFile(file.path)]);
             } else {
+              final name = getName() + applicationFileExtension;
               final location = await getSaveLocation(suggestedName: name);
 
               if (location != null) {
